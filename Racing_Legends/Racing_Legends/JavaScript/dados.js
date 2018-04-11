174c627464d2863b1e5c21aba233ae8ef2c218c6
Daniel <aluno18939@ipt.pt>
2018-04-11 12:31:03 +0100

    Exercicio 0 - criaçao de divs e mostragem de categorias

---
+++ Racing_Legends/Racing_Legends/JavaScript/dados.js
@@ -0,0 +1,14 @@
+﻿// Ficheiro de funções de acesso a dados
+
+function getCategorias() {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories";
+
+    return fetch(url, { headers: { Accept: 'application/json' } })
+		.then(function (resposta) {
+		    if (resposta.status === 200) {
+		        return resposta.json();
+		    } else {
+		        return Promise.reject(new Error("Erro ao obter categorias"));
+		    }
+		});
+}
---
+++ Racing_Legends/Racing_Legends/JavaScript/programacao.js
@@ -0,0 +1,68 @@
+﻿// Ficheiro da aplicação
+
+document.addEventListener('DOMContentLoaded', function main(e) {
+    function mostraCategorias(categorias) {
+        var cats=document.querySelector("#listaCategorias");
+        for (var i = 0; i < categorias.length; i++) {
+            // Criar elementos HTML, colocá-los no body ou noutro lado qualquer.
+            console.log(categorias[i]);
+            var containerCats = document.createElement("div");
+            //vai buscar o nome das categorias
+            var nomeCat = document.createElement("div");
+            nomeCat.textContent = categorias[i].name;
+            containerCats.appendChild(nomeCat);
+
+            
+            //vai buscar descrição das categorias
+            var descriptCats = document.createElement("div");
+            descriptCats.textContent = categorias[i].description;
+            containerCats.appendChild(descriptCats);
+
+            //vai buscar o ID das categorias
+            var IdCats = document.createElement("div");
+            IdCats.textContent = categorias[i].id;
+            containerCats.appendChild(IdCats);
+
+
+            //coloca os dados no div que defini no html (div para lista de categorias)
+            cats.appendChild(containerCats);
+        }
+    }
+
+
+    function ecraCategorias() {
+        return getCategorias()
+		    .then(function (categorias) {
+		        mostraCategorias(categorias);
+		    })
+		    .catch(function (erro) {
+		        console.error(erro);
+		    });
+    }
+
+    ecraCategorias();
+
+});
+
+//Exemplo do eventListener do exercio anterior
+/*
+input.addEventListener("input", function (evt) {
+            var filtroFaixas = input.value;
+
+            //faixasContainer.innerHTML = '';
+            divFaixas.remove();
+
+            if (filtroFaixas.length == 0) {
+                var fxs = cd.querySelectorAll('faixa');
+                //faixasContainer.appendChild(faixas(fxs));
+                divFaixas = faixas(fxs);
+            } else {
+                var faixasFiltradas = cd.querySelectorAll('faixa[ref*="' + filtroFaixas + '"]');
+                //faixasContainer.appendChild(faixas(faixasFiltradas));
+                divFaixas = faixas(faixasFiltradas);
+            }
+
+            albumContainer.appendChild(divFaixas);
+
+        });
+*/
